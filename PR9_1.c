6363f1ea7e87456fee47888c60d6d1bac077f200
prince-buha <150029430+prince-buha@users.noreply.github.com>
2024-01-01 11:33:46 +0530

    Update PR9_1.c

--- PR9_1.c
+++ PR9_1.c
@@ -1,3 +1,14 @@
+/*
+	Q.1 Write a Program to write even & odd numbers from 50 to 70 into two separate files.
+For example, 
+Output:
+Even numbers from even_file.txt:
+50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70
+
+Odd numbers from odd_file.txt:
+51, 53, 55, 57, 59, 61, 63, 65, 67, 69
+
+	*/
 #include<stdio.h>
 void main(){
 	int i;
